7ccead507cc9b75ffd7f4326dcb91386a824079f
Helgi Gunnarsson <helgiorn@gmail.com>
2020-09-02 13:32:25 +0200

    fixing bugs

--- pymodoro.py
+++ pymodoro.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from win10toast import ToastNotifier
 import time
 import datetime as dt
+import webbrowser
+
+
 
 # Collect time information
 time_now = dt.datetime.now()                                       
@@ -27,15 +30,21 @@ def start_pomodoro():
     print("Pomodoro Initiated at: "+ time_now.strftime("%H:%M"))
     print("Pomodoro will finish at : "+ time_future.strftime("%H:%M"))
     print("Pomodoro break will finish at : "+ time_finish.strftime("%H:%M"))
+    #Optional, open noisli
+    #webbrowser.open('https://www.noisli.com/s/96927ffffb')
 
 start_pomodoro()
 # Main script
 while True:
     if time_now < time_future:
-        print('Still in pomomode')
+        time_left = time_future - time_now
+        print (time_left)
+
     elif time_future <= time_now <= time_finish:
         print('Break time!')
         pomodoro_toaster(break_message)
+        #The code can take a break as well
+        time.sleep(delta_sec)
     else:
         pomodoro_toaster("Break Finished.\nGo to shell to start a new Pomodoro!")
         usr_ans = input("Timer has finished. \nWould you like to start another pomodoro? \nY/N:  ")
@@ -46,6 +55,6 @@ while True:
         elif usr_ans == "N":
             print("Thank you!")
             break
-    time.sleep(20)
+    time.sleep(60)
     time_now = dt.datetime.now()
     timenow = time_now.strftime("%H:%M")
